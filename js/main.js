b487061ff657c09cfa2d8d139608af28e739ead9
Aung Thu <athu.mdy@gmail.com>
2014-03-21 17:24:26 +0800

    callback

--- js/main.js
+++ js/main.js
@@ -1,49 +1,10 @@
 $(document).ready(function(){
 	
-	$("#flip").click(function(){
-		
-    	$("#panel").slideToggle("slow");
- 	});
-
-	$("#btnFadeIn").click(function(){
-    	$("#div1").fadeIn();
-    	$("#div2").fadeIn("slow");
-    	$("#div3").fadeIn(3000);
- 	});
-
- 	$("#btnFadeOut").click(function(){
-    	$("#div1").fadeOut();
-    	$("#div2").fadeOut("slow");
-    	$("#div3").fadeOut(3000);
- 	});
-
-
-	$("#btnToggle").click(function(){
-    	$("p").toggle("slow");
- 	});
-
-	$("#btnDbclick").hover(function(){
-		alert('enter mouse');
-	},function(){
-		alert('leave mouse');
-	}
-	);
-
-	$("#btnHideThis").click(function(){
-    	$(this).toggle();
- 	});
-
-	$("#btnHide").click(function(){
-		$("#pHide").hide();
-	});
-
-	$("#btnShow").click(function(){
-		$("#pHide").show();
-	});
-
-	$("#btnHideList1").click(function(){
-		$("ul li:first").hide("slow");
-	});
+	$("button").click(function(){
+    	$("p").hide("slow",function(){
+      alert("The paragraph is now hidden");
+    	});
+  	});
 
 	$("tr:even").css("background-color","gray");
 
